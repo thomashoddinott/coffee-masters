5ebdba1910be4b1a661261b837a4680d91d50942
Thomas Hoddinott <thomashoddinott477@gmail.com>
2022-08-07 20:44:19 +0200

    add a product and update some styles

--- lib/pages/menupage.dart
+++ lib/pages/menupage.dart
@@ -1,10 +1,51 @@
 import 'package:flutter/material.dart';
 
+import '../datamodel.dart';
+
 class MenuPage extends StatelessWidget {
   const MenuPage({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    return Container();
+    var p = Product(
+      id: 1,
+      name: "Black Coffee",
+      price: 1.25,
+      image: "image",
+    );
+    return ProductItem(product: p);
+  }
+}
+
+class ProductItem extends StatelessWidget {
+  final Product product;
+
+  const ProductItem({Key? key, required this.product}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      padding: const EdgeInsets.all(8),
+      child: Card(
+        elevation: 4,
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            Image.asset("images/black_coffee.png"),
+            Padding(
+              padding: const EdgeInsets.all(8.0),
+              child: Text(
+                product.name,
+                style: const TextStyle(fontWeight: FontWeight.bold),
+              ),
+            ),
+            Padding(
+              padding: const EdgeInsets.all(8.0),
+              child: Text("\$${product.price}"),
+            ),
+          ],
+        ),
+      ),
+    );
   }
 }
