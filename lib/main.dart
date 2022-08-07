a2aa41705c32d9e35c5aab0015bbae14f851e730
Thomas Hoddinott <thomashoddinott477@gmail.com>
2022-08-07 20:25:17 +0200

    render more pages

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,6 @@
-import 'package:coffee_masters/offerspage.dart';
+import 'package:coffee_masters/pages/menupage.dart';
+import 'package:coffee_masters/pages/offerspage.dart';
+import 'package:coffee_masters/pages/orderpage.dart';
 import 'package:flutter/material.dart';
 
 void main() {
@@ -83,17 +85,17 @@ class _MyHomePageState extends State<MyHomePage> {
 
   @override
   Widget build(BuildContext context) {
-    Widget currentWidgetPage = Text("...");
+    Widget currentWidgetPage = const Text("...");
 
     switch (selectedIndex) {
       case 0:
-        currentWidgetPage = const Text("Menu Page");
+        currentWidgetPage = const MenuPage();
         break;
       case 1:
         currentWidgetPage = const OffersPage();
         break;
       case 2:
-        currentWidgetPage = const Text("Orders Page");
+        currentWidgetPage = const OrderPage();
         break;
     }
     return Scaffold(
