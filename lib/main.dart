97b3948311160437485a39481e674a23a451aba1
Thomas Hoddinott <thomashoddinott477@gmail.com>
2022-08-07 17:06:17 +0200

    init

---
+++ lib/main.dart
@@ -0,0 +1,90 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyApp());
+  // runApp(const HelloWorld());
+}
+
+class Greet extends StatefulWidget {
+  const Greet({Key? key}) : super(key: key);
+
+  @override
+  State<Greet> createState() => _GreetState();
+}
+
+String name = "";
+
+class _GreetState extends State<Greet> {
+  @override
+  Widget build(BuildContext context) {
+    var greetStyle = const TextStyle(fontSize: 24);
+    return Column(
+      children: [
+        Padding(
+          padding: const EdgeInsets.all(24.0),
+          child: Row(
+            children: [
+              Text(
+                "Hello $name",
+                style: greetStyle,
+              ),
+              Text("!!!", style: greetStyle),
+            ],
+          ),
+        ),
+        Padding(
+          padding: const EdgeInsets.all(24.0),
+          child: TextField(
+              onChanged: (value) => setState(() {
+                    name = value;
+                  })),
+        ),
+      ],
+    );
+  }
+}
+
+class HelloWorld extends StatelessWidget {
+  const HelloWorld({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return const Text("Hello World");
+  }
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: const MyHomePage(title: 'Flutter Demo Home Page'),
+    );
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+  const MyHomePage({Key? key, required this.title}) : super(key: key);
+  final String title;
+
+  @override
+  State<MyHomePage> createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(widget.title),
+      ),
+      body: Greet(),
+    );
+  }
+}
