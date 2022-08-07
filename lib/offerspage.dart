0d8237b08f1307a47bad0d4066fe18d52e79945a
Thomas Hoddinott <thomashoddinott477@gmail.com>
2022-08-07 17:52:01 +0200

    creating the card layout

--- lib/offerspage.dart
+++ lib/offerspage.dart
@@ -25,23 +25,42 @@ class Offer extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Column(children: [
-      Padding(
+    return SizedBox(
+      height: 150,
+      child: Padding(
         padding: const EdgeInsets.all(8.0),
-        child: Center(
-            child: Text(
-          title,
-          style: Theme.of(context).textTheme.headline5,
-        )),
+        child: Card(
+          color: Colors.amber.shade300,
+          elevation: 7,
+          child: Container(
+            decoration: const BoxDecoration(
+              image: DecorationImage(
+                  fit: BoxFit.cover,
+                  image: AssetImage(
+                    "images/background.png",
+                  )),
+            ),
+            child: Column(children: [
+              Padding(
+                padding: const EdgeInsets.all(8.0),
+                child: Center(
+                    child: Text(
+                  title,
+                  style: Theme.of(context).textTheme.headline5,
+                )),
+              ),
+              Padding(
+                padding: const EdgeInsets.all(8.0),
+                child: Center(
+                    child: Text(
+                  description,
+                  style: Theme.of(context).textTheme.headline6,
+                )),
+              ),
+            ]),
+          ),
+        ),
       ),
-      Padding(
-        padding: const EdgeInsets.all(8.0),
-        child: Center(
-            child: Text(
-          description,
-          style: Theme.of(context).textTheme.headline6,
-        )),
-      ),
-    ]);
+    );
   }
 }
