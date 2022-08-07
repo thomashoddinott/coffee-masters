4afcb7d880329fc3f4ee1280e8ae718b7c29f748
Thomas Hoddinott <thomashoddinott477@gmail.com>
2022-08-07 17:25:14 +0200

    init OffersPage

---
+++ lib/offerspage.dart
@@ -0,0 +1,19 @@
+import 'package:flutter/material.dart';
+
+class OffersPage extends StatelessWidget {
+  const OffersPage({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
+
+class Offer extends StatelessWidget {
+  const Offer({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
